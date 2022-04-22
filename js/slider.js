4cc7459d457db16db2a8c8ff285a7bf5e88e5462
LowMaster <79407666+RealLowMaster@users.noreply.github.com>
2022-04-22 20:35:51 +0430

    Create slider.js

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+class Slider {
+	constructor() {
+		this.list = []
+		this.active = []
+		this.container = []
+		this.element = []
+	}
+
+	Open(list, index = null) {
+
+	}
+
+	Prev() {}
+
+	Next() {}
+
+	Change(index) {}
+
+	Close() {}
+}
