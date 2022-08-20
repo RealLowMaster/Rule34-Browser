47f932c2b678170d3b8b7dbaa22cb0f24f27bb7b
RealLowMaster <wamofmaster@gmail.com>
2022-08-20 14:10:02 +0430

    Release v2.3.5

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function createWindow () {
 		show: false,
 		backgroundColor: "#fff",
 		paintWhenInitiallyHidden: true,
-		title: 'Rule34 Browser v2.3.0',
+		title: 'Rule34 Browser v2.3.5',
 		webPreferences: {
 			enableRemoteModule: true,
 			nodeIntegration: true,
